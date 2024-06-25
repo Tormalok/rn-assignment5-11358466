d2666e0e90493ac47fa14102c73f608b3b2587da
Henry Amponsah <hamponsah010@st.ug.edu.gh>
2024-06-25 07:22:44 +0000

    completed the settings screen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      <HomeScreen />
+      <StatusBar style="auto" />
+      {/* <HomeScreen /> */}
+      <SettingsScreen />
     </View>
   );
 }
---
+++ screens/SettingsScreen.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Switch,
+  ScrollView,
+} from 'react-native';
+
+const SettingsScreen = () => {
+  const [isThemeEnabled, setIsThemeEnabled] = useState(false);
+  const toggleSwitch = () =>
+    setIsThemeEnabled((previousState) => !previousState);
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Settings</Text>
+
+      <TouchableOpacity style={styles.option}>
+        <Text style={styles.optionText}>Language</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.option}>
+        <Text style={styles.optionText}>My Profile</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.option}>
+        <Text style={styles.optionText}>Contact Us</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.option}>
+        <Text style={styles.optionText}>Change Password</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.option}>
+        <Text style={styles.optionText}>Privacy Policy</Text>
+      </TouchableOpacity>
+
+      <View style={[styles.option, styles.disableBB]}>
+        <Text style={[styles.optionText, styles.boldText]}>Theme</Text>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isThemeEnabled ? '#f5dd4b' : '#f4f3f4'}
+          onValueChange={toggleSwitch}
+          value={isThemeEnabled}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 50,
+    alignSelf: 'center',
+  },
+  option: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  optionText: {
+    fontSize: 18,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  disableBB: {
+    borderBottomWidth: 0,
+  },
+});
+
+export default SettingsScreen;
